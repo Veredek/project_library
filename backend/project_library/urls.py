8068c7739cfc23e016174ddd50e014876265eb24
Hugo <hugoribeiro.me@gmail.com>
2026-05-06 08:24:25 -0300

    style(backend): rename dj_library to project_library

--- backend/project_library/urls.py
+++ backend/project_library/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for dj_library project.
+URL configuration for project_library project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
